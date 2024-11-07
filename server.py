2ae150d4675619c539cda029ef8a9af6fc6fb970
João Lopes <jp.lopes060303@gmail.com>
2024-11-07 16:28:03 +0000

    listar e pedir uma transmissão

--- server.py
+++ server.py
@@ -4,13 +4,14 @@ import pickle
 import json
 import queue
 import os
+import re
 
 from RTPPacket import RTPPacket
 
 UDP_PORT = 9090
 
 clientsQueue = queue.Queue()
-#nodeQueue?
+nodeQueue = queue.Queue()
 
 #ex: O1 : ipO1, neighboursO1, isPP
 def discoverOverlay():
@@ -47,16 +48,23 @@ def receivePackets(udpSocket):
     while True:
         data, addr = udpSocket.recvfrom(1024) #??
         loadedData = pickle.loads(data)
-        # eventualmente algo para diferenciar de cliente para morte de node
+        
         clientsQueue.put((loadedData, addr))
 
 
 def handleClients(udpSocket,videoList):
-    clientPacket, clientAddr = clientsQueue.get(True) # bloqueia até ter um pacote
+    while True:
+        clientPacket, clientAddr = clientsQueue.get(True) # bloqueia até ter um pacote
 
-    if clientPacket == "VL": #Video List
-        udpSocket.sendto(pickle.dumps(videoList),clientAddr)    
-
+        if clientPacket == "VL": #Video List
+            udpSocket.sendto(pickle.dumps(videoList),clientAddr)    
+        elif (videoName := re.match(r"W : (.*)",clientPacket)):
+            print(f"{clientAddr[0]} => {videoName.group(1)}") #grupo de captura, apagar depois
+            if videoName.group(1) in videoList:
+                udpSocket.sendto(pickle.dumps(f"Recebi o pedido da stream {videoName.group(1)}"),clientAddr) # apagar isto depois
+                # transmitir video numa nova thread
+            else:
+                udpSocket.sendto(pickle.dumps(f"Stream doesn't exist."),clientAddr)
 
 def main():
     nodeDict = discoverOverlay()
@@ -64,8 +72,11 @@ def main():
     udpSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     udpSocket.bind((nodeDict["O1"][0],UDP_PORT))
 
+    #servidor tem conexão tcp com os nodos overlay ligados a ele, manda controlPacket pela rede overlay para obter métricas para os pontos de presença, os pontos de presença comunicam com o cliente e obtêm métricas que enviam para o servidor
+    #o servidor junta as duas e escolhe o melhor ponto de presença para transmitir para o cliente
+
     threading.Thread(target=receivePackets, args=(udpSocket,)).start()
-    threading.Thread(target=handleClients,args=(udpSocket,videoList,)).start()
+    handleClients(udpSocket,videoList)
 
 
 main()
